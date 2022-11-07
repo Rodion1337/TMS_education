498ff783273a9e90add50a24d5a99fecce6e2299
Radik1337Blr <radikmotocross@gmail.com>
2022-11-08 01:34:48 +0300

    L4 fix

---
+++ L4/L4_1.py
@@ -0,0 +1,5 @@
+a = list(input('Введите число: '))
+sum_a = 0
+for c in range(0,len(a)):
+    sum_a += int(a[c])
+print(sum_a)
---
+++ L4/L5_4.py
@@ -0,0 +1,5 @@
+a = set(input('ввежите слово №1: '))
+b = set(input('ввежите слово №2: '))
+c = set(input('ввежите слово №3: '))
+print(sorted(a&b&c))
+print(sorted(a|b|c))
