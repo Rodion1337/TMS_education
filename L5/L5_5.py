03821f808a59cde305fc948012941edc961b6139
Radik1337Blr <radikmotocross@gmail.com>
2022-11-10 01:34:38 +0300

    fianl

--- L5/L5_5.py
+++ L5/L5_5.py
@@ -1,6 +1,5 @@
-#text = input('Введите строку для подсчета повторения символов: ').lower()
-text = 'Oh, it is Python'.lower()
+text = input('Введите строку для подсчета повторения символов: ').lower()
+#text = 'Oh, it is Python'.lower() ## проверка работоспособности по образцу
 c = ''.join(sorted(set(text)))
 d = {a: text.count(a) for a in c}
-print(text)
 print(d)
