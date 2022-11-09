4080aeaa33df6a7c9276f023a0cbc52471c06610
Radik1337Blr <radikmotocross@gmail.com>
2022-11-10 00:03:41 +0300

    clean

--- L4/L4_6.py
+++ L4/L4_6.py
@@ -1,7 +1,4 @@
 text = input('Введите строку: ')
-#text = 'Я ЛюбЛю ЛюДей'
 text = text.capitalize().split(' ')
-#text = text.split(' ')
 text1 = str(text[len(text)-1]).capitalize()
-#text1 = text1.capitalize()
 print(' '.join(text[0:-1]) + ' ' + text1)
