a07a11c7f96ab4254e0550014e7adcb87eb05528
Radik1337Blr <radikmotocross@gmail.com>
2022-11-09 23:38:23 +0300

    final

---
+++ L4/L4_7.py
@@ -0,0 +1,12 @@
+#text = input ('Введите текст (строку): ')
+#x = input ('Введите произвольный символ: ')
+#n = input ('Введите число: ')
+text = 'я люблю людей'
+x = ':'
+n = '10'
+c = ''
+for q in range(1,len(text),2):
+    c += text[q:(q+1)] + n
+for q in range(0,len(text),2):
+    c += text[q:(q+1)] + x
+print(text + '\n' + c + '\n' + str('!'*int(n)))
