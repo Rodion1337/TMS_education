45806d812fd806106969b110ddfd0f1faab861b7
Radik1337Blr <radikmotocross@gmail.com>
2022-11-23 02:06:27 +0300

    L8_2

--- L8/L8_2.py
+++ L8/L8_2.py
@@ -1,20 +1,19 @@
-"""2. Создать скрипт который получает от пользователя 4 строки с данными (данные произвольные)
-Создать файл и записать в него первые 2 строки (после данной операции закрыть файл)
-Преобразовать оставшиеся 2 строки в строки в верхнем регистре и добавить их в созданный файл
-В итоговом файле должно быть 4 строки каждая из которых оканчивается символом « | » и начинается с новой строки
-"""
 #name = input('введите ваше имя: ')
 #color = input(f'{name}, какой цвет твой любимый? ')
 #city = input(f'{name}, из какого ты города? ')
 #bar = input(f'{name}, как называется лучший бар в {city}? ')
 
-name = 'Radik1337Blr'
-color = 'Green'
-city = 'Minsk'
-bar = 'на дне'
+name = ('Radik1337Blr')
+color = ('Green')
+city = ('Minsk')
+bar = ('na dne')
 
-x = input()
-files = open('files.txt', 'wt')
-print (x, file = files)
+with open('files_8_2.txt', 'w') as file:
+    data = [name, color]
+    for line in data:
+        file.write(line + "|\n")
 
-files.write(name)
+with open('files_8_2.txt', 'a') as file:
+    data = [city, bar]
+    for line in data:
+        file.write(line.upper() + "|\n")
