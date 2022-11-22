945d79aade67ffeb041556b38050ca64fedff4ab
Radik1337Blr <radikmotocross@gmail.com>
2022-11-23 02:08:11 +0300

    l8_2 final test

--- L8/L8_2.py
+++ L8/L8_2.py
@@ -1,12 +1,12 @@
-#name = input('введите ваше имя: ')
-#color = input(f'{name}, какой цвет твой любимый? ')
-#city = input(f'{name}, из какого ты города? ')
-#bar = input(f'{name}, как называется лучший бар в {city}? ')
+name = input('введите ваше имя: ')
+color = input(f'{name}, какой цвет твой любимый? ')
+city = input(f'{name}, из какого ты города? ')
+Fbar = input(f'{name}, как называется лучший бар в {city}? ')
 
-name = ('Radik1337Blr')
-color = ('Green')
-city = ('Minsk')
-bar = ('na dne')
+#name = ('Radik1337Blr')
+#color = ('Green')
+#city = ('Minsk')
+#bar = ('na dne')
 
 with open('files_8_2.txt', 'w') as file:
     data = [name, color]
