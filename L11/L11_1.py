6d920a8168d58c7dc5dca5d71c872cbfa3332d54
Rodion Kotaliov <radikmotocross@gmail.com>
2022-12-08 15:09:03 +0300

    L11_1 up

--- L11/L11_1.py
+++ L11/L11_1.py
@@ -15,7 +15,7 @@ class Dish():
     """
     Создание блюд
     """
-    name : str
+    name_dish : str
     price : int
     weight : int
 
@@ -23,25 +23,41 @@ dish_1 = Dish('Цезарь', 15, 150)
 dish_2 = Dish('Пюре', 5, 100)
 dish_3 = Dish('Котлета', 8, 70)
 dish_4 = Dish('Картофель фри', 5, 100)
-
+menu = []
+menu.append(dish_1)
+menu.append(dish_2)
+menu.append(dish_3)
+menu.append(dish_4)
+print(menu)
 
 
 class Order():
-    
-    def __init__(self, name, price, weight):
-        self.name = name
+    def __init__(self, name_dish, price, weight):
+        self.name_dish = name_dish
         self.price = price
         self.weight = weight
 
-    def to_pay(*args): #подсчет суммы к оплате
+    def to_pay(*args) -> int:
+        """подсчет суммы к оплате"""
         to_pay_order = 0
         for i in args:
             to_pay_order = to_pay_order + i.price
         return to_pay_order
 
-summ_to_pay = Order.to_pay(dish_1, dish_2)
-print(summ_to_pay)
+    def balance() -> int:
+        you_pay = input(f'Сумма вашего заказа {Order.to_pay}')
+        pay = Order.to_pay - you_pay
 
-class client():
-    def __init__(self, name):
-        self.name = name
+class Client():
+    def __init__(self):
+        self.name = input('добрый день.\nкак вас зовут?')
+
+    def my_order():
+        
+        x = input(f'будете что-то заказывать? (Y / N)').upper()
+        if x = 'Y':
+            my_dish = input(f'на выбор есть Dish_1: {dish_1.name_dish}, ценна:{dish_1.price}, вес блюда {dish_1.weight}. Будете заказывать? (Y / N)')
+                if my
+
+start = Client()
+
