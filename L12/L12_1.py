fdc600cf420216b63af921466bd6d26fbd37df7f
Rodion Kotaliov <radikmotocross@gmail.com>
2022-12-08 17:23:51 +0300

    L12_1 update

--- L12/L12_1.py
+++ L12/L12_1.py
@@ -1,34 +1,26 @@
-# match_input = input ('Введите формулу, следущеюго вида "2 + 2", "22 / 11"')
-
-match_input = ('1 2 1')
-a = match_input.split(' ')
-
-try:
-    if a[1] in ('+', '-', '/', '*'):
-        print(a[1])
-        if a[1] == '+':
-            print (int(a[0]) + int(a[2]))
-        elif a[1] == '-':
-            print (int(a[0]) - int(a[2]))
-        elif a[1] == '/':
-            print (int(a[0]) / int(a[2]))
-        elif a[1] == '*':
-            print (int(a[0]) * int(a[2]))
+x = ''
+while x != 'yes':
+    match_input = input ('Введите формулу, следующего вида "2 + 2", "22 / 11": ')
+    # match_input = ('1 2 1')
+    a = match_input.split(' ')
+    try:
+        if a[1] in ('+', '-', '/', '*', '**'):
+            print (a[1])
+            if a[1] == '+':
+                print (float(a[0]) + float(a[2]))
+            elif a[1] == '-':
+                print (float(a[0]) - float(a[2]))
+            elif a[1] == '/':
+                print (round(float(a[0]) / float(a[2]), 2))
+            elif a[1] == '*':
+                print (float(a[0]) * float(a[2]))
+            elif a[1] == '**':
+                print (float(a[0]) ** float(a[2]))
+            else:
+                print ('InputFormulaErrorQ')
         else:
             print ('InputFormulaErrorQ')
-except IndexError:
-    print ('InputFormulaErrorQ')
+    except IndexError:
+        print ('InputFormulaErrorQ')
+    x = input('завершить вычисления? (yes / no)\n').lower()
 
-
-    # if a[1] in ('+', '-', '/', '*'):
-    #     print(a[1])
-    # if a[1] == '+':
-    #     print (int(a[0]) + int(a[2]))
-    # elif a[1] == '-':
-    #     print (int(a[0]) - int(a[2]))
-    # elif a[1] == '/':
-    #     print (int(a[0]) / int(a[2]))
-    # elif a[1] == '*':
-    #     print (int(a[0]) * int(a[2]))
-    # else:
-    #         print ('InputFormulaErrorQ')
