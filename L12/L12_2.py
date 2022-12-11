e9194033f313ea803cf427a345bf835cc2b27bbd
Rodion Kotaliov <radikmotocross@gmail.com>
2022-12-11 18:46:47 +0300

    L12_2 final

--- L12/L12_2.py
+++ L12/L12_2.py
@@ -35,28 +35,48 @@ class Validator():
         self.email = email
 
 
-    def validate_login():
-        """не менее 6 символов"""
+    def validate_login(self) -> bool:
+        """login — не менее 6 символов"""
         if len(self.login) < 6:
             raise InvalidLogin('Длина логина менее 6 знаков!')
         else:
             return True
 
 
-    def validate_password():
-        from string import ascii_letters
-        x = ascii_letters
-        print (x)
-        """не менее 8 символов, буквы в верхнем и нижнем регистре, не менее одного специального символа (+-/*! и т.д)"""
-        if len(self.password) < 8:
-                raise InvalidPassword ('Длина пароля менее 8 знаков')
+    def validate_password(self) -> bool:
+        from string import ascii_lowercase, ascii_uppercase, punctuation
+        """password — не менее 8 символов, буквы в верхнем и нижнем регистре, не менее одного специального символа (+-/*! и т.д)"""
+        if len(self.password) >= 8 and len(set(self.password) & set(ascii_lowercase)) > 1 and len(set(self.password) & set(ascii_uppercase)) > 1 and len(
+            set(self.password) & set(punctuation)) > 1:
+                return True
         else:
-            print('exit')
+            raise InvalidPassword ('Ошибка. Пароль не соответствует следующим требованиям: не менее 8 символов, буквы в верхнем и нижнем регистре, не менее одного специального символа')
+
+    def validate_email(self) -> bool:
+        """email — присутствует символ @, оканчивается . и 2 символами (.by)"""
+        email_zone = {'.by','.com','.ru','.io','.net'}
+        # print(self.email[(len(self.email) - self.email[::-1].find('.') - 1):])
+        if '@' in self.email and self.email[(len(self.email) - self.email[::-1].find('.') - 1):] in email_zone:
+            return True
+        else:
+            raise InvalidPassword ('Ошибка. Email не соответствует следующим требованиям:  присутствует символ @, оканчивается . и 2 символами (.by)')
+
+    def validate(self) -> bool:
+        try:
+            self.validate_login()
+            self.validate_password()
+            self.validate_email()
+        except (InvalidLogin, InvalidEmail, InvalidPassword):
+            return False
+        else:
+            return True
 
 
-    def validate_email():
-        pass
 
-pass_valid = Validator()
-x = pass_valid()
-print(pass_valid.validate_login)
+
+
+pass_valid = Validator('Radik1337Blr','qwe!@#QWEzxc','radikmotocross@gmail.com')
+print(pass_valid.validate())
+
+pass_valid2 = Validator('Radik1337Blr','qwe!@#QWEzxc','radikmotocross@gmail.com').validate()
+print(pass_valid2)
