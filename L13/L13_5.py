d08516d8f0c3ff4d74ec53f3a3081d8b02e66407
Rodion Kotaliov <radikmotocross@gmail.com>
2022-12-13 03:06:21 +0300

    fix L13 2-5 (условия ДЗ)

---
+++ L13/L13_5.py
@@ -0,0 +1,8 @@
+"""5*. Реализуйте генератор, который будет бесконечно 
+генерировать числа Фибоначчи (https://en.wikipedia.org/wiki/Fibonacci_number).
+Пример:
+>>> gen = endless_fib_generator()
+>>> while True:
+	print(next(gen))
+>>> 1 1 2 3 5 8 13 ...
+"""
