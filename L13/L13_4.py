6f282292de64d8ad04792a58cbd5c11c65d96be9
Rodion Kotaliov <radikmotocross@gmail.com>
2022-12-15 03:20:07 +0300

    L13_4 final

--- L13/L13_4.py
+++ L13/L13_4.py
@@ -19,3 +19,38 @@ next(er1)
     print(number)
 >>> 4 6 8 10 12 "Out of numbers!"
 """
+
+class EvenRange():
+    def __init__(self, iter_start: int, iter_finish: int) -> int:
+        self.iter_start = iter_start
+        self.iter_finish = iter_finish
+        self.index = 0
+
+    def __next__(self):
+        if self.iter_start < self.iter_finish:
+            if self.iter_start % 2 == 0:
+                self.iter_start += 2
+            else:
+                self.iter_start += 1
+            return self.iter_start
+        else:
+            print ("Out of numbers!")
+            raise StopIteration
+
+    def __iter__(self):
+        return self
+
+
+
+
+er1 = EvenRange(7,11)
+next(er1)
+next(er1)
+next(er1)
+
+er2 = EvenRange(3, 14)
+for number in er2:
+    print(number)
+
+x = EvenRange(3,14)
+print(list(x))
