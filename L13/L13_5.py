b755178333265358464b5f71cbd9a2918a3d9cc2
Rodion Kotaliov <radikmotocross@gmail.com>
2022-12-15 03:24:10 +0300

    L13_5 final

--- L13/L13_5.py
+++ L13/L13_5.py
@@ -6,7 +6,10 @@
 	print(next(gen))
 >>> 1 1 2 3 5 8 13 ...
 """
+fib1, fib2 = 0, 1
 while True:
 	for i in range(2,):
-    	fib1, fib2 = fib2, fib1 + fib2
-    	print(fib2, end=' ')
+		fib1, fib2 = fib2, fib1 + fib2
+		print(fib2, end=' ')
+	# if fib2 >= 100:
+	# 	break 
