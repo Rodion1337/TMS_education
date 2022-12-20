b46830794d1804a887138262b0cbe2a56b0d31c8
Rodion Kotaliov <radikmotocross@gmail.com>
2022-12-20 16:01:16 +0300

    up

--- L13/L13_5.py
+++ L13/L13_5.py
@@ -8,8 +8,8 @@
 """
 fib1, fib2 = 0, 1
 while True:
-	for i in range(2,):
-		fib1, fib2 = fib2, fib1 + fib2
-		print(fib2, end=' ')
+	# for i in range(2,):
+	fib1, fib2 = fib2, fib1 + fib2
+	print(fib2, end=' ')
 	# if fib2 >= 100:
 	# 	break 
