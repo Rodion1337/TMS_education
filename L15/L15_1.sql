58be291f6d62320ff0bbd7cfc4977a96d7308acf
Rodion Kotaliov <radikmotocross@gmail.com>
2022-12-21 17:42:11 +0300

    L15_1 final

--- L15/L15_1.sql
+++ L15/L15_1.sql
@@ -3,6 +3,7 @@ SELECT Composer, sum(bytes) as "Размер" FROM tracks group by Composer;
 
 
 """2) Сколько записей находится в таблицах employees и customers"""
+SELECT sum(count_for_sum) FROM (SELECT count(*) as count_for_sum from employees UNION SELECT count(*) as count_for_sum from customers)
 
 
 """3) Получить список треков tracks из альбома «A-Sides»"""
@@ -10,3 +11,4 @@ SELECT count(name) as 'A-Sides' FROM tracks where AlbumId = (SELECT AlbumId FROM
 
 
 """4) Используя группировку (https://www.w3schools.com/sql/sql_groupby.asp) определите общую стоимость треков в каждом альбоме"""
+SELECT albums.Title, sum(tracks.UnitPrice) FROM albums JOIN tracks on albums.AlbumId = tracks.AlbumId GROUP by albums.Title
