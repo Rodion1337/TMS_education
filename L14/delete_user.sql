5067a06153b4ea7ddb003542fcc689dc173c7cf9
Rodion Kotaliov <radikmotocross@gmail.com>
2022-12-22 00:03:51 +0300

    fix

--- L14/delete_user.sql
+++ L14/delete_user.sql
@@ -1,3 +1,10 @@
-DELETE FROM users WHERE user_id = 2;
+-- DELETE FROM users WHERE user_id = 2;
 
 """Попытался удалить юзера, но почему-то не получается ни как"""
+
+
+ALTER TABLE posts ADD CONSTRAINT post_author_id FOREIGN KEY(post_author_id) REFERENCES users(user_id) ON DELETE CASCADE;
+DELETE FROM users WHERE user_id = 2;
+
+FOREIGN KEY(post_author_id) REFERENCES users(user_id) ON DELETE SET NULL;
+DELETE FROM users WHERE user_id = 2;
