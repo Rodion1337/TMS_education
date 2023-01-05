f7e4fb9a51528075741b6baf09e29e1f61eb6167
Rodion Kotaliov <radikmotocross@gmail.com>
2023-01-05 03:31:09 +0300

    up

--- L18/app.py
+++ L18/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, render_template
 from datetime import datetime
+import requests
 
 # def create_app():
 #     app = Flask(__name__)
@@ -12,23 +13,31 @@ from datetime import datetime
 
 app = Flask(__name__)
 # app.config.from_object('config.DevelopmentConfig')
-
+navigation = [{'link':'/', 'name':'Главная страница'},
+    {'link':'about', 'name':'О сайте'},
+    {'link':'time', 'name':'Время'},
+    {'link':'kanye_west','name':'цитата Kanye West'}]
 
 @app.route('/index')
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html', navigation = navigation)
 
 @app.route('/time')
 def time_out():
-    return datetime.now().isoformat()
+    return render_template('time.html', navigation = navigation, time = datetime.now().isoformat())
 
-@app.route('/test')
-def test():
-    navigation = [{'link':'/', 'name':'Главная страница'},
-    {'link':'about', 'name':'О сайте'},
-    {'link':'time', 'name':'Время'}]
-    return render_template('test.html', navigation = navigation)
+@app.route('/about')
+def about():
+    return render_template('about.html', navigation = navigation)
+
+print(requests.get('https://api.kanye.rest'))
+
+@app.route('/kanye_west')
+def kanye_west():
+    spoiler = requests.get('https://api.kanye.rest')
+    print(spoiler)
+    return render_template('kanye_west.html', navigation = navigation, spoiler = spoiler)
 
 
 if __name__ == '__main__':
