2768f54a4c17d55dc6645fbe1fbf3c9fcc7e40fd
Rodion Kotaliov <radikmotocross@gmail.com>
2023-01-09 02:03:39 +0300

    fix

--- L15/L15_2.py
+++ L15/L15_2.py
@@ -3,25 +3,20 @@
 
 from peewee import *
 
+# find_album = input('Введите название альбома, трека которго необходимо вывести: ')
 
-def input_tracks():
-    find_album = input('Введите название альбома, трека которго необходимо вывести: ')
+def input_tracks(find_album):
+    # find_album = input('Введите название альбома, трека которго необходимо вывести: ')
     # find_album = "Jagged Little Pill"
 
     find_order = (f'SELECT tracks.Name FROM albums JOIN tracks on albums.AlbumId = tracks.AlbumId WHERE albums.Title = "{find_album}"')
     
     bd = SqliteDatabase(r'L15\chinook.db')
-    cursor = bd.cursor()
-    cursor.execute(find_order)
-    return(cursor.fetchall())
-    bd.close()
-    
+    with bd:
+        cursor = bd.cursor()
+        cursor.execute(find_order)
+        return(cursor.fetchall())
 
 
 
-trecks = input_tracks()
-print(trecks)
-
-# class BaseModel(Model): 
-# 	class Meta: 
-# 		database = bd
+print(input_tracks('A-Sides'))
