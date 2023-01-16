2c76c747eae93cc353858c82a900268d8d25b5a6
Rodion Kotaliov <radikmotocross@gmail.com>
2023-01-17 01:43:39 +0300

    fix

--- L20/testDjango/testDjango/settings.py
+++ L20/testDjango/testDjango/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'testApp.apps.TestappConfig',
+    'bboard.apps.BboardConfig',
 ]
 
 MIDDLEWARE = [
