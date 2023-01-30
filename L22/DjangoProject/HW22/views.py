5c78740d8b99ed7c482178a7272c59e3bf3e7be0
Radik1337Blr <radikmotocross@gmail.com>
2023-01-30 05:27:48 +0300

    fix

--- L22/DjangoProject/HW22/views.py
+++ L22/DjangoProject/HW22/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+def index(requset):
+    return HttpResponse('Hello world!')
+
+def about(requset):
+    return HttpResponse('Hello world!')
