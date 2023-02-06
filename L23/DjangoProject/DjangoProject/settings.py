5a8d6b8350504e2caedf1d7b6c1dbc9defe3f354
Radik1337Blr <radikmotocross@gmail.com>
2023-02-06 03:03:38 +0300

    fix

--- L23/DjangoProject/DjangoProject/settings.py
+++ L23/DjangoProject/DjangoProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'HW23.apps.Hw23Config',
 ]
 
@@ -78,7 +79,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    'postgresql': { 
+		'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+		'NAME': 'GameShop_HW23', 
+		'USER': 'Radik1337Blr', 
+		'PASSWORD': '6303', 
+		'HOST': '127.0.0.1', 
+		'PORT': '5432'
+	}
 }
 
 
@@ -104,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
--- L23/DjangoProject/HW23/models.py
+++ L23/DjangoProject/HW23/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+class GamesDev(models.Model):
+    nameDev = models.CharField(verbose_name='Разработчик', max_length=50, db_index=True)
+    description = models.TextField(verbose_name='Описание',blank=True, null=True)
+    
+    class Meta:
+        verbose_name = 'Разработчик'
+        verbose_name_plural = 'Разработчики'
+        ordering = ['nameDev']
+    
+    def __str__(self) -> str:
+        return self.nameDev
+
+class categories(models.Model):
+    title = models.CharField(verbose_name = 'Категория', max_length=50)
+    slug = models.SlugField(verbose_name=("Адрес"))
+    description = models.TextField(verbose_name=("Описание"))
+    is_active = models.fields.BooleanField(verbose_name='Доступность')
+    
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+    
+    def __str__(self) -> str:
+        return self.title
+
+class games(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Игра', db_index=True)
+    pub_date = models.DateField(auto_now_add=True, verbose_name='Дата публикации')
+    upd_date = models.DateField(auto_now=True, verbose_name='Дата обновления')
+    release_date = models.DateField(verbose_name='Дата релиза')
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name = 'Цена', default = 0)
+    gameDev = models.ForeignKey(GamesDev, verbose_name = 'Издатель', on_delete = models.PROTECT)
+    slug = models.SlugField()
+    category = models.ForeignKey("categories", verbose_name = 'Категория', on_delete = models.PROTECT)
+    description = models.TextField(verbose_name = 'Описание игры')
+    game_image = models.ImageField(verbose_name = 'Логотип игры', upload_to = 'HW23/images', height_field=None, width_field=None, max_length=None)
+    is_active_game = models.fields.BooleanField(verbose_name='Доступность')
+    
+    class Meta:
+        verbose_name = 'Игра'
+        verbose_name_plural = 'Игры'
+        ordering = ['name']
