f9b6b42336c05781b13dc83d727887af236b49ae
Radik1337Blr <radikmotocross@gmail.com>
2023-02-13 04:12:01 +0300

    bug fix

--- L23/DjangoProject/HW23/urls.py
+++ L23/DjangoProject/HW23/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('?=<sort>', views.index, name='index'),
-    path('game/', views.game_views, name = 'game_views'),
     path('game/<slug:game_slug>', views.game_views, name = 'game_views'),
     path('category/<slug:category>', views.category_views, name = 'categories'),
 
