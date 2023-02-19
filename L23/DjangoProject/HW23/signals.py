055ead41eda44430d3420dca4621eb62585dafe0
Radik1337Blr <radikmotocross@gmail.com>
2023-02-20 01:44:53 +0300

    HW24 final

--- L23/DjangoProject/HW23/signals.py
+++ L23/DjangoProject/HW23/signals.py
@@ -1,41 +1,41 @@
-from .models import games
+from .models import Games, Categories
 from django.dispatch import receiver
 from django.db.models.signals import post_save, pre_delete, pre_save
+from string import punctuation, ascii_letters, digits
 
-@receiver(post_save, sender = games)
+@receiver(post_save, sender = Games)
 def game_amount_up(sender, instance, created, **kwargs):
     instance = instance.category
     if created:
         instance.game_amount += 1
         instance.save()
-    if instance.slug == 'null':
-        x = instance.name
-        instance.slug = '_'.join(x.lower().split(' '))
-        instance.save()
 
 
-@receiver(pre_delete, sender = games)
+@receiver(pre_delete, sender = Games)
 def game_amount_up(sender, instance, **kwargs):
     instance = instance.category
     instance.game_amount -= 1
     instance.save()
         
-@receiver(pre_save, sender = games)   
+@receiver(pre_save, sender = Categories)
+@receiver(pre_save, sender = Games)
 def upd_slug(sender, instance, **kwargs):
     if instance.slug == None:
         x = instance.name
-        # text = (i for i in x if i.isalnum() or i == ' ')
-        # print (text)
-        # instance.slug = '_'.join(str(text).lower().split(' '))
-        # dic = {'Ь':'', 'ь':'', 'Ъ':'', 'ъ':'', 'А':'A', 'а':'a', 'Б':'B', 'б':'b', 'В':'V', 'в':'v',
-        #     'Г':'G', 'г':'g', 'Д':'D', 'д':'d', 'Е':'E', 'е':'e', 'Ё':'Yo', 'ё':'yo', 'Ж':'Zh', 'ж':'zh',
-        #     'З':'Z', 'з':'z', 'И':'I', 'и':'i', 'Й':'Y', 'й':'y', 'К':'K', 'к':'k', 'Л':'L', 'л':'l',
-        #     'М':'M', 'м':'m', 'Н':'N', 'н':'n', 'О':'O', 'о':'o', 'П':'P', 'п':'p', 'Р':'R', 'р':'r', 
-        #     'С':'S', 'с':'s', 'Т':'T', 'т':'t', 'У':'U', 'у':'u', 'Ф':'F', 'ф':'f', 'Х':'H', 'х':'h',
-        #     'Ц':'Ts', 'ц':'ts', 'Ч':'Ch', 'ч':'ch', 'Ш':'Sh', 'ш':'sh', 'Щ':'Sch', 'щ':'sch', 'Ы':'Yi',
-        #     'ы':'yi', 'Э':'E', 'э':'e', 'Ю':'Yu', 'ю':'yu', 'Я':'Ya', 'я':'ya'}
-        # translate = ''
-        # for i in instance.slug:
-        #     translate += dic.get(i.lower(), i.lower()).upper() if i.isupper() else dic.get(i, i)
-        # instance.slug = translate
-        instance.slug = '_'.join(x.lower().split(' '))
+        dic = {'Ь':'', 'ь':'', 'Ъ':'', 'ъ':'', 'А':'A', 'а':'a', 'Б':'B', 'б':'b', 'В':'V', 'в':'v',
+            'Г':'G', 'г':'g', 'Д':'D', 'д':'d', 'Е':'E', 'е':'e', 'Ё':'Yo', 'ё':'yo', 'Ж':'Zh', 'ж':'zh',
+            'З':'Z', 'з':'z', 'И':'I', 'и':'i', 'Й':'Y', 'й':'y', 'К':'K', 'к':'k', 'Л':'L', 'л':'l',
+            'М':'M', 'м':'m', 'Н':'N', 'н':'n', 'О':'O', 'о':'o', 'П':'P', 'п':'p', 'Р':'R', 'р':'r', 
+            'С':'S', 'с':'s', 'Т':'T', 'т':'t', 'У':'U', 'у':'u', 'Ф':'F', 'ф':'f', 'Х':'H', 'х':'h',
+            'Ц':'Ts', 'ц':'ts', 'Ч':'Ch', 'ч':'ch', 'Ш':'Sh', 'ш':'sh', 'Щ':'Sch', 'щ':'sch', 'Ы':'Yi',
+            'ы':'yi', 'Э':'E', 'э':'e', 'Ю':'Yu', 'ю':'yu', 'Я':'Ya', 'я':'ya', ' ':'_'}
+        name_slug = ''
+        text_free = ascii_letters + digits
+        for i in instance.name:
+            if i in text_free:
+                name_slug += i
+            elif i in punctuation:
+                pass
+            else:
+                name_slug += str(dic[i])
+        instance.slug = name_slug.lower()
