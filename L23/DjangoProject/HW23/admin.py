cf483324cdd9d0fb9bf5cadb7a3213c15139378b
Radik1337Blr <radikmotocross@gmail.com>
2023-02-26 04:56:45 +0300

    avg price HW26

--- L23/DjangoProject/HW23/admin.py
+++ L23/DjangoProject/HW23/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+from django.db.models import Avg
 # Register your models here.
 
 class GamesDevAdmin(admin.ModelAdmin):
@@ -7,15 +8,26 @@ class GamesDevAdmin(admin.ModelAdmin):
     list_display_links = ('nameDev',)
 
 class CategoriesAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'is_active','game_amount',)
+    list_display = ('title', 'description', 'is_active','game_amount', 'average_cost',)
     list_display_links = ('title', 'description', 'is_active',)
     list_filter = ('title', 'description', 'is_active','game_amount',)
+    
+    @admin.display(description="average cost (средняя цена)")
+    def average_cost(self, obj):
+        return round(Games.objects.filter(category = obj).aggregate(Avg('price'))['price__avg'],2)
+
+
 
 class GamesAdmin(admin.ModelAdmin):
-    list_display = ('name', 'gameDev', 'release_date', 'category', 'is_active', 'status', 'slug', 'upd_date',)
+    list_display = ('name', 'gameDev', 'release_date', 'category', 'is_active', 'status', 'slug', 'upd_date', 'img_preview',)
     list_display_links = ('name',)
     search_fields = ('name', 'gameDev', 'release_date', 'category', 'is_active', 'status',)
     fields = ('name', 'gameDev', 'release_date', 'category', 'price', 'description', 'is_active', 'status', 'game_image', 'slug',)
+    
+    @admin.display(description='game image')
+    def img_preview(self, obj):
+        from django.utils.html import mark_safe
+        return mark_safe(f'<img src = "{obj.game_image.url}" height="75px"/>')
 
 class StatusAdmin(admin.ModelAdmin):
     list_display = ('status_name',)
--- L23/DjangoProject/HW23/views.py
+++ L23/DjangoProject/HW23/views.py
@@ -24,12 +24,23 @@ def game_views(request, game_slug = None):
         return render(request, 'games.html', context={'games' : games_view,})
 
 def category_views(request, category = None):
+    
+    sorted_game = request.GET.get('sort', 'None')
+    sorted_order = {
+        'None': Games.objects.all(),
+        'price': Games.objects.order_by('price'),
+        'name': Games.objects.order_by('name'),
+    }
+    # games_sorted = sorted_order.get(sorted_game).all()
+    # categories_sorted = list(Categories.objects.all())
+    # return render(request, 'index.html', context={'games': games_sorted, "categories": categories_sorted})
     if category != None:
-        category = Categories.objects.get(title = category)   
-        games_view = Games.objects.filter(category = category.id)
+        # category = Categories.objects.get(title = category)   
+        games_view = Games.objects.filter(category = Categories.objects.get(title = category).id)
         return render(request, 'games.html', context={'games' : games_view,})
     else:
         games_view = Games.objects.all()
         return render(request, 'games.html', context={'games' : games_view,})
+
 
 
