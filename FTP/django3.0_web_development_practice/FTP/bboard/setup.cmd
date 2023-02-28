3d9cd8c9afbb23c1f78b21389921742150aea1d1
Radik1337Blr <radikmotocross@gmail.com>
2023-02-28 05:22:07 +0300

    .

--- FTP/django3.0_web_development_practice/FTP/bboard/setup.cmd
+++
@@ -1,7 +0,0 @@
-pip install django
-pip install django-bootstrap4
-pip install easy-thumbnails
-pip install django-cleanup
-pip install django-simple-captcha
-pip install djangorestframework
-pip install django-cors-headers
