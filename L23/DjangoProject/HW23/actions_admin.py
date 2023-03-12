f9fe586c40520efda7a3c1f58ecf1589fce95b76
Radik1337Blr <radikmotocross@gmail.com>
2023-03-12 18:56:08 +0300

    дополнил по "замечанию" к ДЗ26

--- L23/DjangoProject/HW23/actions_admin.py
+++ L23/DjangoProject/HW23/actions_admin.py
@@ -27,7 +27,15 @@ def export_as_csv(self, request, queryset):
     response['Content-Disposition'] = 'attachment; filename={}.csv;'.format(datetime.datetime.now())
     response.write(u'\ufeff'.encode('utf8')) #обязательный элемент для корректной выгрузки в utf8
     writer = csv.writer(response)
-    writer.writerow(['name', 'slug', 'pub_date', 'release_date', 'price', 'category', 'gameDev', 'status', 'description'])
-    for i in queryset:
-        writer.writerow([i.name, i.slug, i.pub_date, i.release_date, i.price, i.category, i.gameDev, i.status, i.description])
+    # writer.writerow(['name', 'slug', 'pub_date', 'release_date', 'price', 'category', 'gameDev', 'status', 'description'])
+    # for i in queryset:
+    #     writer.writerow([i.name, i.slug, i.pub_date, i.release_date, i.price, i.category, i.gameDev, i.status, i.description])
+    
+    from django.db.models import Field
+    writer.writerow([field.name for field in self.model._meta.get_fields() if isinstance(field, Field)])  
+    #- тут первым рядом пишем заголовок из названий полей модели
+    for i in queryset: #- тут проходим циклом по каждой записи в запросе
+        writer.writerow([getattr(i,field.name) for field in i._meta.get_fields() if isinstance(field, Field)]) 
+        #- тут для записи получаем значение поля через getattr, выражением списка формируем строку записи и writer`ом пишем ее
+    
     return response
